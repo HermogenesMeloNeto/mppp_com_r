33a63447d4822f4ee081fb0d979dc3ca5abe784f
Hermógenes de Melo Neto <hermogenes.m.n@gmail.com>
2022-09-08 12:05:52 -0300

    Exercício Funções de Repetição - Família Apply

---
+++ scripts/08-02_Funções de Repetição - Família Apply.R
@@ -0,0 +1,9 @@
+# funções de repetição - família apply
+# Uso da mapply com uma customização melhor, gerando títulos personalizados, 
+# incluse inserindo no título a palavra "Histogra da"
+
+par(mfrow = c(2, 2)) # prepara a área de plotagem para receber 4 plots
+
+mapply(hist, iris[ , 1:4], main = paste("Histograma da",names(iris)[1:4]), 
+                           xlab = "Valores da Variável", 
+                           ylab = 'Frequência')
