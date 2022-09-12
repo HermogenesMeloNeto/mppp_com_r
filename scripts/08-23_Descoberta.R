bf61c31cf99d88d766b877dc02d7afe87866c360
Hermógenes de Melo Neto <hermogenes.m.n@gmail.com>
2022-09-11 22:11:26 -0300

    Exercício Descoberta

---
+++ scripts/08-23_Descoberta.R
@@ -0,0 +1,7 @@
+pacman::p_load(funModeling, tidyverse) 
+
+glimpse(facebook) # olhada nos dados
+status(facebook) # estrutura dos dados (missing etc)
+freq(facebook) # frequência das variáveis fator
+plot_num(facebook) # exploração das variáveis numéricas
+profiling_num(facebook) # estatísticas das variáveis numéricas
