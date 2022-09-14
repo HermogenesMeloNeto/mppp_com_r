a361fbf7eb26c98a8fc75343a4b82856810086c3
Hermógenes de Melo Neto <hermogenes.m.n@gmail.com>
2022-09-14 13:04:20 -0300

    Comentários.

--- scripts/09-19_Regressão Linear - Implementação.R
+++ scripts/09-19_Regressão Linear - Implementação.R
@@ -7,6 +7,9 @@ summary(regUSArrests) # SUMÁRIO DA REGRESSÃO
 lm.beta(regUSArrests) # COEFICIENTES PADRONIZADOS
 
 # Stepwise #
+# As variáveis não significativas são excluídas e o modelo econométrico é montado automaticamente. 
+# Há três possibilidade:
+
 regUSArrestsBack <- step(lm(Assault ~ ., data = USArrests), direction = 'backward') # de trás pra frente
 summary(regUSArrestsBack)
 regUSArrestsForw <- step(lm(Assault ~ ., data = USArrests), direction = 'forward') # do início pro fim
@@ -18,9 +21,11 @@ summary(regUSArrestsBoth)
 
 # Possíveis outliers
 boxplot(USArrests$Assault, USArrests$Murder, USArrests$Rape, names = c('Assault', 'Murder', 'Rape'), col = c('red', 'gray', 'gray'), main = 'Boxplot')
+# Não há outliers
 
 # normalidade dos resíduos
-shapiro.test(residuals(regUSArrests))
+shapiro.test(residuals(regUSArrests)) # p-value>0,05, os resíduos são normais
+hist(residuals(regUSArrests)) # Histograma dos resíduos
 plot(regUSArrests, which=1, col=c("blue")) # resíduos x ajuste
 plot(regUSArrests, which=2, col=c("red")) # Q-Q Plot
 plot(regUSArrests, which=5, col=c("blue"))  # Observações Influentes
@@ -32,11 +37,15 @@ plot(regUSArrests, which=5, col=c("blue"))  # Observações Influentes
 vif(regUSArrests)
 barplot(vif(regUSArrests), main = "VIF Values", horiz = FALSE, col = "steelblue", ylim = c(0,5))
 abline(h = 5, lwd = 3, lty = 2)
+# Não há variáveis inflacionadas, todas estão abaixo de 5.
 
 #= Correção seria usar apenas uma das variáveis correlacionadas =#
+# Não há necessidade, visto não há variáfeis inflacionadas.
 
 # homocedasticidade (H0 = homocedasticidade)
-bptest(regUSArrests) # teste de homocedasticidade
+bptest(regUSArrests) # Para passar no teste de homocedasticidade p-valor>0,05
+
+# Homocedasticidade implica não haver tendência para os resídos, que devem oscilar acima e abaixo da curva
 plot(regUSArrests, which=3, col=c("blue"))  # Scale-Location Plot
 
 #= Correção seria usar estimativas robustas =#
@@ -46,3 +55,5 @@ coeftest(regUSArrests, regUSArrests$robse)
 regRUSArrests <- rlm(Assault ~ . -UrbanPop, data = USArrests)
 summary(regRUSArrests)
 summary(regUSArrests)
+
+#Fica sugestão de refazer excluindo o North Carolina, Rhoke Island, Georgia, Delaware, Hawaii
